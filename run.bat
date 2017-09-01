8fe7605eff1d11a52993de759ea031f6e1fa5a9e
mikusjelly <mikusjelly@gmail.com>
2017-09-01 11:28:20 +0800

    update

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 @echo off
-java -classpath build/libs/OracleDriver-1.0.2.jar org.cf.oracle.Driver %*
+java -classpath build/libs/OracleDriver-1.0.2.jar;libs/test.jar org.cf.oracle.Driver %*
--- todex.bat
+++
@@ -1,2 +0,0 @@
-dx --dex --force-jumbo --output=driver.dex build/libs/OracleDriver-1.0.2.jar
-rm -r build
